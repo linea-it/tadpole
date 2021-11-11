fe5fad5a522fd3c1f4174289a79842856eb19b5b
matheusallein <mtsallein@gmail.com>
2021-11-11 17:03:14 -0300

    Updating Program and Information page's content

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Container, Typography } from '@material-ui/core';
+import { Grid, Container, Typography, Link } from '@material-ui/core';
 import useStyles from './styles';
 
 function Home() {
@@ -23,17 +23,27 @@ function Home() {
           </Typography>
         </Grid>
         <Grid item xs={12} className={classes.logos}>
-          <img
-            src="/img/lsst-bpg.jpg"
-            alt="LSST Brazilian Participation Group"
-          />
-          <img src="/img/lsst-c.jpg" alt="LSST Corporation" />
-          <img src="/img/rubin.jpg" alt="Vera C. Rubin" />
-          <img
-            src="/img/oba.jpg"
-            alt="Olimpíada Brasileira de Astronomia e Astronáutica"
-          />
-          <img src="/img/sab.jpg" alt="Sociedade Astronômica Brasileira" />
+          <Link href="https://bpg-lsst.linea.gov.br/" target="_blank">
+            <img
+              src="/img/lsst-bpg.jpg"
+              alt="LSST Brazilian Participation Group"
+            />
+          </Link>
+          <Link href="https://www.lsstcorporation.org/" target="_blank">
+            <img src="/img/lsst-c.jpg" alt="LSST Corporation" />
+          </Link>
+          <Link href="https://www.lsst.org/ " target="_blank">
+            <img src="/img/rubin.jpg" alt="Vera C. Rubin" />
+          </Link>
+          <Link href="http://www.oba.org.br/site/" target="_blank">
+            <img
+              src="/img/oba.jpg"
+              alt="Olimpíada Brasileira de Astronomia e Astronáutica"
+            />
+          </Link>
+          <Link href="https://sab-astro.org.br/" target="_blank">
+            <img src="/img/sab.jpg" alt="Sociedade Astronômica Brasileira" />
+          </Link>
         </Grid>
       </Grid>
     </Container>
